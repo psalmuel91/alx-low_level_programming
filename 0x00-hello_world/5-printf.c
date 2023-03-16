5f265154078525c8795ef66ad02189ac1711375c
Olu-Samuel <olualaofin.o.samuel@gmail.com>
2023-03-16 18:33:32 +0200

    Commits on task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Program writes to the standard output with printf function
+ * Return: return 0 after executing the program
+ * Program compiles without warning when, with the -Wall gcc option
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

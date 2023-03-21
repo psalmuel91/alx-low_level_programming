0324ed2ebbb4767229a335b1b0ba650ea21c7a75
Olu-Samuel <olualaofin.o.samuel@gmail.com>
2023-03-21 20:57:16 +0200

    Adds description to 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
-#include "main.h"
+]#include "main.h"
 
 /**
-* main - print_alphabet followed by a newline
+* print_alphabet: prints alphabets followed by a newline
 *
 * Return: return (0) Success
 */

862cc37cb5b36ab4283ea8547843c1928c2eedec
Olu-Samuel <olualaofin.o.samuel@gmail.com>
2023-03-21 18:39:23 +0200

    Adds a README.md file and a program that prints _printchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <printput.h>
+
+/**
+* main - Prints _putchar followed by a newline
+*
+* Return: return (0) Success
+*/
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+
+}

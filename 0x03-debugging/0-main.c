5f89b5dd733adbde6a18f68bab59a07fabebcb53
Olu-Samuel <olualaofin.o.samuel@gmail.com>
2023-03-22 15:44:22 +0200

    Commits on task 0, adds README.md, header file and program tests positive or negative value

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - Program test function for positive or negative
+*
+* Return: return (0) Success
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}

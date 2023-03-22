375ce8320636e04b125ada41a53544b16ad85ce3
Olu-Samuel <olualaofin.o.samuel@gmail.com>
2023-03-22 16:11:22 +0200

    Edits to header file

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
-* main - Program test function for positive or negative
-*
-* Return: return (0) Success
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
 
 int main(void)

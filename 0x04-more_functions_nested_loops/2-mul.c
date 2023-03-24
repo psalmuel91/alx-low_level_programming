b47d51ebe8d0240c3fc78c3cd12ac8779a4d6253
Olu-Samuel <olualaofin.o.samuel@gmail.com>
2023-03-24 23:33:12 +0200

    Commits on task 2, program computes multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* mul -multiplies two integers
+* @a: argument variable holds int
+* @b: argument variable holds int
+*
+* Return: return (0) Success
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

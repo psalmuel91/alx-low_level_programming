a8c480c67f196b9de8bc24ec63f241d8f3905539
Olu-Samuel <olualaofin.o.samuel@gmail.com>
2023-03-25 00:51:44 +0200

    Commits on task 7, program prints diagonal lines

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+* print_diagonal - program prints diagonal lines
+* @n: argument holds integer variable
+*
+* Return: return (0) Success
+*/
+void print_diagonal(int n)
+{
+	int i;
+	int b;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (b = 0; b < i; b++)
+			{
+				_putchar (' ');
+			}
+			_putchar (92);
+			_putchar ('\n');
+
+		}
+
+	}
+	else
+	{
+		_putchar ('\n');
+	}
+}

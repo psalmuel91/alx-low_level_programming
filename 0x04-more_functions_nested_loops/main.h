31c3c7e86851730138ddd034415407a99d58ef8f
Olu-Samuel <olualaofin.o.samuel@gmail.com>
2023-03-24 22:51:56 +0200

    Commits on task 0, program evaluates character to upper or lowercase

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+
+/**
+* main - All prototype resides here for linkage
+* @c: argument with variable char
+*
+* Return: return (0) Success
+*/
+
+int _putchar(char c);
+int _isupper(int c);
+
+#endif

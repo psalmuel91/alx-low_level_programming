8b9ff554bb67abd690f6588ecc074a92bd311175
Olu-Samuel <olualaofin.o.samuel@gmail.com>
2023-03-25 02:34:48 +0200

    Edits on the main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,20 @@
 
 #include <stdio.h>
 
+/**
+* main - All prototype resides here for linkage
+* @i: argument with variable char
+* @a: argument with variable char
+* @b: argument with variable char
+* @c: argument with variable char
+* @day: argument with variable char
+* @month: argument with variable char
+* @year: argument with variable char
+*
+* Return: return (0) Success
+*/
+
+
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);

d26f13f60dfbfe2dc317387c2c350a64aeecfd71
Olu-Samuel <olualaofin.o.samuel@gmail.com>
2023-03-27 10:08:18 +0200

    Commits on task 0, program updates value of a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* reset_to_98 - Takes a pointer and updates value it points to
+* @n: argument pointer an int to be updated
+*
+* Return: return nothing on success
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

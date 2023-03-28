417b7a3dbd3ae1d260e5a9e2ddb7a2a970dbcd5a
Olu-Samuel <olualaofin.o.samuel@gmail.com>
2023-03-28 21:04:53 +0200

    Commits on task 11, program generates password crack key

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+/**
+ * main - Entry point, program generates and print password.
+ *
+ * Return: return (0) success.
+ */
+
+int main(void)
+{
+	int ascii = 2772, i = 0, j, randNum;
+	char p_word[100];
+	time_t t;
+
+	srand((int) time(&t));
+	while (ascii > 126)
+	{
+		randNum = rand() % 126;
+		p_word[i] = randNum;
+		ascii -= randNum;
+		i++;
+	}
+	if (ascii > 0)
+		p_word[i] = ascii;
+	else
+	{
+		i--;
+	}
+
+
+	for (j = 0; j <= i; j++)
+	{
+		printf("%c", p_word[j]);
+	}
+	return (0);
+}

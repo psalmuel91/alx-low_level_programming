d4fc998dc231c7875f4b2714d52762557bbb6f24
Olu-Samuel <olualaofin.o.samuel@gmail.com>
2023-03-31 22:47:38 +0200

    Commits on task 1, program concatenate strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,12 +4,16 @@
 /**
 * main - All prototype resides here for linkage
 * @c: argument with variable char
+* @dest: pointer argument
+* @src: pointer argument
+* @n: integer n-byte size
 *
 * Return: return (0) Success
 */
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 
 
 #endif

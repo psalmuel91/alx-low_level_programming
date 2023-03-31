ffd4f663320c5748350fa11024a0338b14f64b9b
Olu-Samuel <olualaofin.o.samuel@gmail.com>
2023-03-31 22:27:48 +0200

    Commits on task 0, program concatenate two strings

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+
+/**
+* main - All prototype resides here for linkage
+* @c: argument with variable char
+*
+* Return: return (0) Success
+*/
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+
+
+#endif

d8478146470bb976093fddac90826c5c01621258
Olu-Samuel <olualaofin.o.samuel@gmail.com>
2023-04-04 03:15:20 +0200

    Commits on task 2, program fills memory with a constant byte

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -38,3 +38,234 @@ int main(void)
 	}
 	return (0);
 }
+
+
+/*
+The program initializes a variable ascii to the value 2772, an integer variable i to 0, an integer variable randNum to 0, and a character array p_word with a length of 100.
+
+The program uses the time() function from the time.h library to seed the random number generator with the current time.
+
+The program enters a loop that generates a random integer between 0 and 126, inclusive, and adds it to the character array p_word. The program subtracts the generated integer from the variable ascii, which is initially set to 2772.
+
+The loop continues to generate and add random integers to the p_word array until the value of ascii is less than or equal to 126.
+
+If the final value of ascii is greater than or equal to 0, the program adds it to the p_word array. Otherwise, the program subtracts 1 from the variable i, since the last integer added to p_word caused the value of ascii to become negative.
+
+Finally, the program iterates over the elements of the p_word array and prints them to the console, excluding any null characters.
+
+Overall, the program generates a random password by adding random integers to a character array until a certain threshold is met, then prints the resulting password to the console.
+
+
+
+
+Samuel Oladele Olu-Alaofin
+can you recreate the code in c #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+/**
+ * main - Entry point, program generates and print password.
+ *
+ * Return: return (0) success.
+ */
+
+int main(void)
+{
+	int ascii = 2772, i = 0, j, randNum;
+	char p_word[100];
+	time_t t;
+
+	srand((int) time(&t));
+	while (ascii > 126)
+	{
+		randNum = rand() % 126;
+		p_word[i] = randNum;
+		ascii -= randNum;
+		i++;
+	}
+	if (ascii >= 0)
+		p_word[i] = ascii;
+	else
+	{
+		i--;
+	}
+
+
+	for (j = 0; j <= i; j++)
+	{
+		if (p_word[j] != '\0')
+			printf("%c", p_word[j]);
+	}
+	return (0);
+}
+Sure! Here's the C# version of the code:
+
+csharp
+Copy code
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int ascii = 2772, i = 0, j, randNum;
+        char[] p_word = new char[100];
+        Random rand = new Random();
+
+        while (ascii > 126)
+        {
+            randNum = rand.Next() % 126;
+            p_word[i] = (char)randNum;
+            ascii -= randNum
+
+
+
+Samuel Oladele Olu-Alaofin
+can you recreate this code #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+/**
+ * main - Entry point, program generates and print password.
+ *
+ * Return: return (0) success.
+ */
+
+int main(void)
+{
+	int ascii = 2772, i = 0, j, randNum;
+	char p_word[100];
+	time_t t;
+
+	srand((int) time(&t));
+	while (ascii > 126)
+	{
+		randNum = rand() % 126;
+		p_word[i] = randNum;
+		ascii -= randNum;
+		i++;
+	}
+	if (ascii >= 0)
+		p_word[i] = ascii;
+	else
+	{
+		i--;
+	}
+
+
+	for (j = 0; j <= i; j++)
+	{
+		if (p_word[j] != '\0')
+			printf("%c", p_word[j]);
+	}
+	return (0);
+}
+Certainly, here's the equivalent code in Python:
+
+python
+Copy code
+import random
+
+ascii = 2772
+i = 0
+p_word = []
+while ascii > 126:
+    randNum = random.randint(0, 126)
+    p_word.append(chr(randNum))
+    ascii -= randNum
+    i += 1
+
+if ascii >= 0:
+    p_word.append(chr(ascii))
+else:
+    i -= 1
+
+for j in range(i+1):
+    if p_word[j] != '\0':
+        print(p_word[j], end='')
+print()
+This Python code uses the random module to generate random numbers between 0 and 126, and the chr function to convert those numbers to characters. It also uses a list p_word to store the characters instead of an array of characters in C. Finally, it uses the end='' argument in the print() function to prevent the output from being printed on multiple lines.
+
+
+
+
+Samuel Oladele Olu-Alaofin
+can you reproduce this code in c language #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+/**
+ * main - Entry point, program generates and print password.
+ *
+ * Return: return (0) success.
+ */
+
+int main(void)
+{
+	int ascii = 2772, i = 0, j, randNum;
+	char p_word[100];
+	time_t t;
+
+	srand((int) time(&t));
+	while (ascii > 126)
+	{
+		randNum = rand() % 126;
+		p_word[i] = randNum;
+		ascii -= randNum;
+		i++;
+	}
+	if (ascii >= 0)
+		p_word[i] = ascii;
+	else
+	{
+		i--;
+	}
+
+
+	for (j = 0; j <= i; j++)
+	{
+		if (p_word[j] != '\0')
+			printf("%c", p_word[j]);
+	}
+	return (0);
+}
+Sure, here's the equivalent code in C language:
+
+arduino
+Copy code
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+int main(void)
+{
+    int ascii = 2772, i = 0, j, randNum;
+    char p_word[100];
+    time_t t;
+
+    srand((int) time(&t));
+    while (ascii > 126)
+    {
+        randNum = rand() % 126;
+        p_word[i] = randNum;
+        ascii -= randNum;
+        i++;
+    }
+    if (ascii >= 0)
+        p_word[i] = ascii;
+    else
+    {
+        i--;
+    }
+
+    for (j = 0; j <= i; j++)
+    {
+        if (p_word[j] != '\0')
+            printf("%c", p_word[j]);
+    }
+    return 0;
+}
--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,7 +4,6 @@
 * _strchr - function locates a character in a string
 * @s: Argument pointer
 * @c: Argument pointer
-* @n: Argument variable n holds integer size
 *
 *Return: return s
 */

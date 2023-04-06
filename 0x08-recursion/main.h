35465eb4b21b7aa7565ea1c6d2c0e7507fb327c9
Olu-Samuel <olualaofin.o.samuel@gmail.com>
2023-04-06 13:26:43 +0200

    Commit on task 3, program computes and return the factorial of an integer

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,4 @@
+
 #ifndef MAIN_HEADER_FILE
 #define MAIN_HEADER_FILE
 
@@ -15,6 +16,7 @@ int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
+int factorial(int n);
 
 
 #endif

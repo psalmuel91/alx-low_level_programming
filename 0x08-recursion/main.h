80bc7a1a234326c25ec1b43a5d903e42dd129716
Olu-Samuel <olualaofin.o.samuel@gmail.com>
2023-04-06 13:40:04 +0200

    Commits on task 4, program computes and print the value of a base raised to a power

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,4 +1,3 @@
-
 #ifndef MAIN_HEADER_FILE
 #define MAIN_HEADER_FILE
 
@@ -7,6 +6,8 @@
 * @c: argument with variable char
 * @b: pointer argument
 * @s: pointer argument
+* @x: pointer argument
+* @y: pointer argument
 * @n: integer n-byte size
 *
 * Return: return (0) Success
@@ -17,6 +18,7 @@ void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
+int _pow_recursion(int x, int y);
 
 
 #endif

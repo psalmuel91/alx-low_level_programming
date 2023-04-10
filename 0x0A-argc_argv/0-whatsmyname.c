e4f7fde4dfe2e678e1a721925ab85b5dfb094939
Olu-Samuel <olualaofin.o.samuel@gmail.com>
2023-04-10 20:30:24 +0200

    Edits to printf function, and excluding main.h from my .gitignore file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,21 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
 * main - Program builds a function that fills memory with a constant byte
 * @argc: Argument holds the count of command line arguments
 * @argv: Argument pointer to array containing the program cmd line arguments
 *
-*Return: return (0) on Success.
+* Return: return (0) on Success.
 */
 
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[argc *0]);
+	int i = 0;
+	for (; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
 
 	return (0);
 }

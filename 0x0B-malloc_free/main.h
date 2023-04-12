295f3d44518b998443e6755055b2a6e67f8a17e7
Olu-Samuel <olualaofin.o.samuel@gmail.com>
2023-04-12 19:58:47 +0200

    Commits on task 3 and 4,function that returns a pointer to a 2 dimensional array of integers, and next function that frees a 2 dimensional grid previously created by your alloc_grid function

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,11 +4,12 @@
 /**
 * main - All prototype resides here for linkage
 * @c: argument with variable char
-* @b: pointer argument
+* @str: pointer argument
 * @size: pointer argument
-* @x: pointer argument
-* @y: pointer argument
-* @n: integer n-byte size
+* @s1: pointer argument
+* @s2: pointer argument
+* @grid: integer
+* @height: integer
 *
 * Return: return (0) Success
 */
@@ -17,6 +18,7 @@ int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
-
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
 
 #endif

3d697cc42cbf0a4d316e9aac8c95a55c3b7aca43
Olu-Samuel <olualaofin.o.samuel@gmail.com>
2023-04-12 16:20:35 +0200

    Edit to files

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+* rot13 - Program encodes a string
+* @str: Argument pointer variable holds streams of char
+*
+* Return: return on success
+*/
+
+char *rot13(char *str)
+{
+	int idx1 = 0, idx2;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+			     'G', 'H', 'I', 'J', 'K', 'L',
+			     'M', 'N', 'O', 'P', 'Q', 'R',
+			     'S', 'T', 'U', 'V', 'W', 'X',
+			     'Y', 'Z', 'a', 'b', 'c', 'd',
+			     'e', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'p',
+			     'q', 'r', 's', 't', 'u', 'v',
+			     'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			     'T', 'U', 'V', 'W', 'X', 'Y',
+			     'Z', 'A', 'B', 'C', 'D', 'E',
+			     'F', 'G', 'H', 'I', 'J', 'K',
+			     'L', 'M', 'n', 'o', 'p', 'q',
+			     'r', 's', 't', 'u', 'v', 'w',
+			     'x', 'y', 'z', 'a', 'b', 'c',
+			     'd', 'e', 'f', 'g', 'h', 'i',
+			     'j', 'k', 'l', 'm'};
+
+	while (str[idx1])
+	{
+		for (idx2 = 0; idx2 < 52; idx2++)
+		{
+			if (str[idx1] == alphabet[idx2])
+			{
+				str[idx1] = rot13key[idx2];
+				break;
+			}
+		}
+
+		idx1++;
+
+	}
+	return (str);
+}

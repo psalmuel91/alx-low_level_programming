e6681fefee6212aff6cf0620087dc4a2df28130e
Olu-Samuel <olualaofin.o.samuel@gmail.com>
2023-04-12 16:58:04 +0200

    Commits on task 0, creates an array of chars, and initializes it with a specific char

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+
+/**
+* main - All prototype resides here for linkage
+* @c: argument with variable char
+* @b: pointer argument
+* @size: pointer argument
+* @x: pointer argument
+* @y: pointer argument
+* @n: integer n-byte size
+*
+* Return: return (0) Success
+*/
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+
+#endif

13eae1958317ba3b5f21df13d657a646de2de6c9
Olu-Samuel <olualaofin.o.samuel@gmail.com>
2023-04-18 19:38:56 +0200

    Commits on task 3, program defines a new type dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,16 +8,16 @@
 * @owner: Pointer variable names the owner of the dog
 */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif

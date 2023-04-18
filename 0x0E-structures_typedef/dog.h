43486a38d3783446d65a9c0c4582fbc5dce8e5bd
Olu-Samuel <olualaofin.o.samuel@gmail.com>
2023-04-18 18:55:03 +0200

    Commits on task 0, program defines a new datatype dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_HEADER_FILE
+#define DOG_HEADER_FILE
+
+/**
+* struct dog - A new datatype describing a dog
+* @name: Pointer variable names the dog
+* @age: Variable age, holds the age of the dog
+* @owner: Pointer variable names the owner of the dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif

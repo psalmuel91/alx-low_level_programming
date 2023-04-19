6d6985905fd53d5f8b837510ba7ebd4f9db64dbf
Olu-Samuel <olualaofin.o.samuel@gmail.com>
2023-04-20 01:07:51 +0200

    Commits on task 3, program performs simple operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Adds two numbers
+ * @x: First Number
+ * @y: Second Number
+ *
+ * Return: Sum of a and b
+ */
+int op_add(int x, int y)
+{
+	return (x + y);
+}
+
+/**
+ * op_sub - Substracts two numbers
+ * @a: First Number
+ * @b: Second Number
+ *
+ * Return: Substraction of a and b
+ */
+
+int op_sub(int x, int y)
+{
+	return (x - y);
+}
+
+/**
+ * op_mul - Multiplies two numbers
+ * @a: First Number
+ * @b: Second Number
+ *
+ * Return: Multiplication of a and b
+ */
+
+int op_mul(int x, int y)
+{
+	return (x * y);
+}
+
+/**
+ * op_div - Divides two numbers
+ * @a: First Number
+ * @b: Second Number
+ *
+ * Return: Division of a and b
+ */
+int op_div(int x, int y)
+{
+	return (x / y);
+}
+
+/**
+ * op_mod - Modulus of two numbers
+ * @a: First Number
+ * @b: Second Number
+ *
+ * Return: Modulus of a and b
+ */
+int op_mod(int x, int y)
+{
+	return (x % y);
+}

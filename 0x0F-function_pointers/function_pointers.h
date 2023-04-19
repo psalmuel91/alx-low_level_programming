1de784888d9db4cf67f4c871513e9b800101e8ba
Olu-Samuel <olualaofin.o.samuel@gmail.com>
2023-04-19 15:26:16 +0200

    Commits on task 0, function prints character to stdout

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+
+/**
+* main - All prototype resides here for linkage
+* @c: argument with variable char
+*
+* Return: return (0) Success
+*/
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+
+#endif

55afe95f2647493b20212746b84c31fe440725cc
Olu-Samuel <olualaofin.o.samuel@gmail.com>
2023-04-28 03:33:35 +0200

    Removes the formatting in 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,11 @@
-/**
-* Description - A 64-bit program in assembly that
-* prints Hello, Holberton.
-*/
-
-extern printf
-
-section .text
-   global main
+section	.text
+	global main
+	extern printf
 
 main:
-   mov edi, msg
-   mov eax, 0
-   call printf
+	mov edi, msg
+	mov eax, 0
+	call printf
 
 section .data
-   msg: db "Hello, Holberton", 0
+	msg: db "Hello, Holberton", 0

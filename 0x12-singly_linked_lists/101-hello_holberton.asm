e1221e55e91dae250af63196d33100e5332fb267
Olu-Samuel <olualaofin.o.samuel@gmail.com>
2023-04-28 03:37:17 +0200

    Removes the formatting in 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,19 @@
 section	.text
-	global main
 	extern printf
+	global main
 
 main:
+	push rbp
+
+	mov esi, fmt
 	mov edi, msg
 	mov eax, 0
 	call printf
 
+	pop rbp
+	mov eax, 0
+	ret
+
 section .data
 	msg: db "Hello, Holberton", 0
+	fmt: db "%s", 10, 0

063031880ebd65d316854eb9585ba97ccc539668
Olu-Samuel <olualaofin.o.samuel@gmail.com>
2024-02-02 04:33:56 +0200

    Commit 1-list_len file from local

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * list_len: list number of elements in
+ *            a linked list_t list.
+ * @h: list_t list.
+ * @nodes: a counter var for nodes
+ *
+ * Return: Number of elements in h.
+ */
+size_t list_len(const list_t *h)
+{
+    size_t nodes;
+
+    nodes = 0;
+    while (h != NULL)
+    {
+        h = h->next;
+        nodes++;
+    }
+    return (nodes);
+}
